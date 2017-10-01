a33064ee34061865c1a8f9eeae61e4033ff5f680
mhelmer@home <martin.helmer@scs.ch>
2017-10-01 23:01:33 +0200

    Deadlock

---
+++ MahAppTabControlIssue/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace MahAppTabControlIssue
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Add.Click += AddOnClick;
+        }
+
+        private void AddOnClick(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var n = Test.Items.Count+1;
+            Test.Items.Add(new MetroTabItem
+            {
+                Header = "ServiceRegistry"
+            });
+        }
+    }
+}
